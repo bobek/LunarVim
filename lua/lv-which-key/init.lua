b53369088f61b4f5e8517fa9a8f8485a2892f985
Antonin Kral <a.kral@bobek.cz>
2021-05-01 08:16:58 +0200

    Change leader

--- lua/lv-which-key/init.lua
+++ lua/lv-which-key/init.lua
@@ -44,8 +44,8 @@ local opts = {
 }
 
 -- Set leader
-vim.api.nvim_set_keymap('n', '<Space>', '<NOP>', {noremap = true, silent = true})
-vim.g.mapleader = ' '
+-- vim.api.nvim_set_keymap('n', '<Space>', '<NOP>', {noremap = true, silent = true})
+vim.g.mapleader = ','
 
 -- no hl
 vim.api.nvim_set_keymap('n', '<Leader>h', ':set hlsearch!<CR>', {noremap = true, silent = true})
